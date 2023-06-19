ba0a2edce7b00cabf62b319207ad0d63f30e978e
dbrandon <django.brandon@gmail.com>
2023-06-18 18:30:57 -0700

    fixed initial login

--- run_server.py
+++ run_server.py
@@ -1,2 +1,5 @@
 
-print('RUnning server...')
+from server.server_main import OTApp, Old_Times_Version
+
+print('Run server ' + str(Old_Times_Version))
+OTApp.run()
--- server/server_main.py
+++ server/server_main.py
@@ -1,10 +1,13 @@
 
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, flash, request
 from flask_login import LoginManager, login_required
+
+import flask_login
 
 from .ot_user_manager import UserManager
 
 import os
+import sys
 
 OTRoot = os.getcwd()
 
@@ -17,7 +20,40 @@ OTApp.secret_key = 'secret key'
 OTLoginManager = LoginManager()
 OTLoginManager.init_app(OTApp)
 
+OTUserManager = UserManager()
+
 @OTLoginManager.user_loader
 def load_user(user_id):
-    return UserManager.lookup_user(user_id)
+    return OTUserManager.lookup_user(user_id)
 print('hello')
+
+@OTApp.route('/')
+@flask_login.login_required
+def get_home_page():
+  flash('Welcome ' + flask_login.current_user.name + '!  Prepare to die!')
+  return render_template('start_page.html', version = Old_Times_Version)
+
+@OTApp.route('/get_authorized')
+def get_authorized():
+  print('Request authorization!')
+  print(flask_login.current_user)
+
+  if flask_login.current_user.is_authenticated != True:
+    user = OTUserManager.establish_user()
+    flask_login.login_user(user)
+    print('User: ID=' + flask_login.current_user.get_id() + ', Auth=' + str(flask_login.current_user.is_authenticated) + ', Name=' + flask_login.current_user.name)
+    flash('You didn\'t provide a name.  I think I\'ll call you... ' + flask_login.current_user.name)
+  else:
+    print('Already authorized, redirecting ' + flask_login.current_user.name + ' to authtest')
+    sys.stdout.flush()
+    return redirect(url_for('get_home_page'))
+  
+  next = request.args.get('next')
+  print('next is [' + str(next) + ']')
+  sys.stdout.flush()
+  return redirect(next or url_for('get_home_page'))
+
+@OTLoginManager.unauthorized_handler
+def unauthorized_handler():
+  print('flak login unauthorized!')
+  return redirect(url_for('get_authorized'))
