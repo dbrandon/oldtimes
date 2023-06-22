41d5161434043bd88919fb9e5644e82bf5bf6ab7
dbrandon <django.brandon@gmail.com>
2023-06-21 18:15:34 -0700

    addition of scenario, addition of angular tutorial

--- server/ot_scenario.py
+++ server/ot_scenario.py
@@ -1,6 +1,154 @@
 
+import namegenerator
+import random
+
+class Item:
+  def __init__(self, name:str) -> None:
+    self._name = name
+
+  @property
+  def name(self) -> str:
+    return self._name
+  
+
+class Creature:
+  def __init__(self, name:str) -> None:
+    self._name = name
+    self._stats = dict[str,int]()
+    self._var = dict[str,int]()
+    self._inventory = list[Item]()
+
+    self.health = 5
+    self.mana = 0
+
+  def attack(self, other : 'Creature') -> None:
+    hit = random.random() > 0.4
+    if not hit:
+      return self.name + ' swings at ' + other.name + ' but misses!'
+    
+    damage = int(random.random() * 4)
+    if damage < 1:
+      return self.name + ' hits ' + other.name + ' with a glancing blow'
+    
+    health = other.health - damage
+    if health < 0:
+      health = 0
+    other.health = health
+
+    if health < 1:
+      return self.name + ' hits ' + other.name + ' for ' + str(damage) + ' points of damage, killing ' + other.name
+    return self.name + ' hits ' + other.name + ' for ' + str(damage) + ' points'
+  
+  @property
+  def is_alive(self) -> bool:
+    return self.health > 0
+  
+  @property
+  def name(self) -> str:
+    return self._name
+  
+  @property
+  def health(self) -> int:
+    return self._var['hp']
+  
+  @health.setter
+  def health(self, hp:int):
+    self._var['hp'] = hp
+  
+  @property
+  def mana(self) -> int:
+    return self._var['mp']
+  
+  @mana.setter
+  def mana(self, mp:int):
+    self._var['mp'] = mp
+      
+
+class Monster(Creature):
+  def __init__(self, name: str, kind:int) -> None:
+    super().__init__(name)
+    self._kind = kind
+    self.init_stats()
+
+  def init_stats(self):
+    self.health = 15
+
+
+class PartyMember(Creature):
+  def __init__(self, name: str, cls:str) -> None:
+    super().__init__(name)
+    self._class = cls
+    self.init_stats()
+
+  def init_stats(self) -> None:
+    if self._class == 'warrior':
+      self._stats['str'] = 20
+      self._stats['int'] =  5
+      self._stats['end'] = 14
+    elif self._class == 'cleric':
+      self._stats['str'] = 12
+      self._stats['int'] = 14
+      self._stats['end'] =  9
+    else:
+      raise 'Unknown class: [' + self._class + ']'
+    
+    self.health = self._stats['end'] * 2
+    self.mana = self._stats['int']
+
+  @property
+  def cls(self) -> str:
+    return self._class
+    
 
 class OTScenario:
   def __init__(self, name:str) -> None:
-    self.name = name
+    self._name = name
+    self._party = OTScenario.create_party()
+
+    self._monsters = list[Monster]()
+    self._monsters.append(Monster(OTScenario.create_name(), 1))
+
+    self._finished = False
+
     pass
+
+  def create_party() -> list[PartyMember]:
+    party = list[PartyMember]()
+
+    party.append(PartyMember(OTScenario.create_name(), 'warrior'))
+    party.append(PartyMember(OTScenario.create_name(), 'cleric'))
+    return party
+
+  def create_name() -> str:
+    first_name = random.choice(namegenerator.LEFT).capitalize()
+    last_name = random.choice(namegenerator.RIGHT).capitalize()
+    return first_name + ' ' + last_name
+  
+  def attack(self) -> list[str]:
+    result = list[str]()
+
+    if not self.monsters[0].is_alive:
+      result.append('There are no monsters left to kill!')
+      return result
+    
+    result.append(self.party[0].attack(self.monsters[0]))
+    if not self.monsters[0].is_alive:
+      result.append('All opponents have been vanquished!')
+      self._finished = True
+    return result
+
+  @property
+  def is_finished(self) -> bool:
+    return self._finished
+
+  @property
+  def name(self) -> str:
+    return self._name
+  
+  @property
+  def party(self) -> list[PartyMember]:
+    return self._party
+  
+  @property
+  def monsters(self) -> list[Monster]:
+    return self._monsters
--- server/server_main.py
+++ server/server_main.py
@@ -1,6 +1,7 @@
 
-from flask import Flask, redirect, url_for, render_template, flash, request
-from flask_login import LoginManager, login_required
+from flask import Flask, redirect, url_for, render_template, flash, request, jsonify
+from flask_login import LoginManager
+from flask_restful import Resource, Api
 
 import flask_login
 
@@ -22,6 +23,23 @@ OTApp.secret_key = 'secret key'
 OTLoginManager = LoginManager()
 OTLoginManager.init_app(OTApp)
 
+OTApi = Api(OTApp)
+
+class Rsrc1(Resource):
+  def get(self):
+    return {'Hello': 'world'}
+  
+#OTApi.add_resource(Rsrc1, '/rest/hello')
+@OTApp.route('/rest/hello')
+@flask_login.login_required
+def rest_hello():
+  user = get_user()
+  sname = 'None'
+  if user.scenario != None:
+    sname = user.scenario.name
+
+  return jsonify({ 'username': user.name, 'scen_name' : sname})
+
 OTUserManager = UserManager()
 
 @OTLoginManager.user_loader
@@ -31,13 +49,15 @@ def load_user(user_id):
 @OTApp.route('/')
 @flask_login.login_required
 def get_home_page():
-  flash('Welcome ' + flask_login.current_user.name + '!  Prepare to die!')
+  user = get_user()
+  flash('Welcome ' + user.name + '!  Prepare to die!')
+
   return render_template('start_page.html', version = Old_Times_Version)
 
 @OTApp.route('/begin_game')
 @flask_login.login_required
 def begin_game():
-  user : OTUser = flask_login.current_user
+  user = get_user()
 
   if user.scenario == None:
     print('Need to start a scenario!')
@@ -46,6 +66,27 @@ def begin_game():
     print('Scenario already established')
   sys.stdout.flush()
   return render_template('begin_game.html', name = user.name, scenario = user.scenario)
+
+@OTApp.route('/attack')
+@flask_login.login_required
+def party_attack():
+  user = get_user()
+
+  if user.scenario == None:
+    flash('No scenario has been started!')
+    return redirect(url_for('get_home_page'))
+  
+  result = user.scenario.attack()
+  for msg in result:
+    flash(msg)
+  return render_template('begin_game.html', name= user.name, scenario = user.scenario)
+
+@OTApp.route('/reset_scenario')
+@flask_login.login_required
+def reset_scenario():
+  user = get_user()
+  user.set_scenario(None)
+  return redirect(url_for('get_home_page'))
 
 @OTApp.route('/get_authorized')
 def get_authorized():
@@ -66,6 +107,9 @@ def get_authorized():
   print('next is [' + str(next) + ']')
   sys.stdout.flush()
   return redirect(next or url_for('get_home_page'))
+
+def get_user() -> OTUser:
+  return flask_login.current_user
 
 @OTLoginManager.unauthorized_handler
 def unauthorized_handler():
